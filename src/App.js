ff05215eab452ed73496a71df2293daa96c3324e
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2019-06-12 20:33:18 +0500

    adding class component

--- src/App.js
+++ src/App.js
@@ -1,53 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
-import {
-  Button,
-  Form,
-  Grid,
-  Header,
-  Message,
-  Segment
-} from "semantic-ui-react";
+import { Button, Form, Grid, Header, Segment } from "semantic-ui-react";
 import HomeContainer from "./containers/index";
 
-function App() {
-  return (
-    <div className="App">
-      <HomeContainer>
-        <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as="h2" color="teal" textAlign="center">
-            {/* <Image src="/logo.png" /> Log-in to your account */}
-          </Header>
-          <Form size="large">
-            <Segment stacked>
-              <Form.Input
-                fluid
-                icon="user"
-                iconPosition="left"
-                placeholder="Postcode e.g EH48 2GG"
-              />
-              <Form.Input
-                fluid
-                icon="user"
-                iconPosition="left"
-                placeholder="House number, street, sublocality"
-              />
-              <Form.Input
-                fluid
-                icon="lock"
-                iconPosition="left"
-                placeholder="City"
-              />
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <HomeContainer>
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Header as="h2" color="teal" textAlign="center" />
+            <Form size="large">
+              <Segment stacked>
+                <Form.Input
+                  fluid
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="Postcode e.g EH48 2GG"
+                />
+                <Form.Input
+                  fluid
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="House number, street, sublocality"
+                />
+                <Form.Input
+                  fluid
+                  icon="lock"
+                  iconPosition="left"
+                  placeholder="City"
+                />
 
-              <Button color="teal" fluid size="large">
-                Search
-              </Button>
-            </Segment>
-          </Form>
-        </Grid.Column>
-      </HomeContainer>
-    </div>
-  );
+                <Button color="teal" fluid size="large">
+                  Search
+                </Button>
+              </Segment>
+            </Form>
+          </Grid.Column>
+        </HomeContainer>
+      </div>
+    );
+  }
 }
 
 export default App;
