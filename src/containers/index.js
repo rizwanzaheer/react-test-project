f74a285458007b4a62baf9c9218ab7a877f2d434
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2019-06-12 11:37:22 +0500

    separate the style

--- src/containers/index.js
+++ src/containers/index.js
@@ -5,15 +5,13 @@ import { Grid } from "semantic-ui-react";
 class HomeContainer extends Component {
   render() {
     return (
-      <Grid
-        textAlign="center"
-        style={{ height: "100vh" }}
-        verticalAlign="middle"
-      >
+      <Grid textAlign="center" style={styles} verticalAlign="middle">
         {this.props.children}
       </Grid>
     );
   }
 }
+
+const styles = { height: "100vh" };
 
 export default HomeContainer;
